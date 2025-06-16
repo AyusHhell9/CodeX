fdc5e77562e152c38d62e37653fde575da96ea48
AyusHxzz <147822985+AyusHhell9@users.noreply.github.com>
2025-06-16 20:12:22 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+per=int(input('enter value of percentage:'))
+if per>=50:
+    print('first division')
+elif per>=40:
+    print('second division')
+elif per>=30:
+    print('third division')
+else:
+    print('fall')
